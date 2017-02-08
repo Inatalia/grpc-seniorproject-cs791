a5261bb02ade46e38abc4455428f946b60d07bc9
Warren Edgar <wew6@wildcats.unh.edu>
2017-02-08 09:56:20 -0500

    Added the multithreaded test script for REST

---
+++ timeScriptREST_async.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import threading
+
+ITERATIONS = 12
+THREADNUMBER = 8
+times = []
+for x in range(0,ITERATIONS):
+  times.append([])
+  for n in range(0,THREADNUMBER):
+    times[x].append([])
+
+f = open('results.txt','w')
+
+def work(tid, iid, numberOfIterations, timesRef):
+  import time as t
+  import os
+  localTimes = []
+  for n in range( 0 , numberOfIterations):
+    start = t.time()
+    os.system("curl -silent gcom2:8080/rest/hello/intextform >> /dev/null")
+    end = t.time()
+    localTimes.append(end-start)
+  timesRef[iid][tid] = localTimes
+
+for n in range(0,ITERATIONS):
+  threads = []
+  for i in range(0,THREADNUMBER):
+    t = threading.Thread(target=work, args=(i,n,2**n,times))
+    threads.append(t)
+  for x in threads:
+     x.start()
+  for x in threads:
+     x.join()
+  f.write("Iteration number " + str(n) + " : " + str(THREADNUMBER*(2**n)) + " total times\n")
+  for x in range(0,THREADNUMBER):
+    for y in range(0,len(times[n][x])):
+      f.write( str(times[n][x][y])+ "\n")
+
+f.close()
